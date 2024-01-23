e53cd89ec2f28ffbb8ef82e2001059d6f888e530
Galya Bogdanova <galya_bogdanova@MacBook-Air-Maksim.local>
2024-01-23 11:28:01 -0800

    fix: add sync files-copy on deletion from files in 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -8,6 +8,15 @@ async function copyDirectory() {
   const dirCreation = await fs.mkdir(folderCopyPath, { recursive: true });
 
   const files = await fs.readdir(folderPath);
+  const filesCopy = await fs.readdir(folderCopyPath);
+
+  for (const fileCopy of filesCopy) {
+    const fileCopyPath = path.join(folderCopyPath, fileCopy);
+    if (!files.includes(fileCopy)) {
+      await fs.unlink(fileCopyPath);
+    }
+  }
+
   for (const file of files) {
     const filePath = path.join(folderPath, file);
     const fileCopyPath = path.join(folderCopyPath, file);
